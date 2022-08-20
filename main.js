3f6b9161017df45167129c9b0c15d953946c7f85
unknown <dr.hebaelgamil@gmail.com>
2022-08-21 00:32:09 +0200

    finish project

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 let ctx = document.getElementById("chart");
 
 // create a new chart
@@ -36,3 +32,25 @@ let myChart = new Chart(ctx, {
         },
     }
 });
+
+// show or hide sidebar
+const menuBtn= document.querySelector('#menu-btn');
+const closeBtn= document.querySelector('#close-btn');
+const sideBar= document.querySelector('aside');
+
+menuBtn.addEventListener('click', ()=>{
+    sideBar.style.display="block";
+})
+closeBtn.addEventListener('click', ()=>{
+    sideBar.style.display="none";
+})
+
+//change theme
+const themeBtn= document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark-theme');
+})
+
+themeBtn.querySelector('span:first-child').classList.toggle('active');
+themeBtn.querySelector('span:last-child').classList.toggle('active');
